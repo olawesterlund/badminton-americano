dd106098988e4ff0cd60d8eecbea37e1c6b9c402
Ola Westerlund <ola.westerlund1@ingka.ikea.com>
2023-02-03 21:55:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project to shedule badminton in the style of padel americano
---
+++ src/algo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def calculate_schedule(no_of_pairs: int) -> list:
+    
+    print("Hello World!")
+    return [] 
+
+def main():
+    calculate_schedule(8)
+
+if __name__ == "__main__":
+    main()
